c6e9c1b42e375fdafe3412a57673ebe28e70684a
marthamwende <marthamwende38@gmail.com>
2021-03-19 12:51:41 +0300

    trainingtest2 index.php
    Training test 2

--- index.php
+++ index.php
@@ -70,6 +70,117 @@ if ($text == "1"){
     $response .= "00. Main Menu \n";
     $response .= "000. End Session \n";
 }
+else if ($text == "0") {
+    $response .= "CON Hi, Welcome to The Kenya School of Government. \n";
+    $response .= "Please select the area you'd like to give feedback on \n";
+    $response .= "1. Training \n";
+    $response .= "2. Dining \n";
+    $response .= "3. Accomodation \n";
+    $response .= "4. Conferencing \n";
+    $response .= "5. Library services \n";
+    $response .= "6. Security \n";
+    $response .= "0. Back \n";
+    $response .= " \n";
+    $response .= "00. Main Menu \n";
+    $response .= "000. End Session \n";
+}
+else if ($text == "00") {
+    $response .= "CON Hi, Welcome to The Kenya School of Government. \n";
+    $response .= "Please select the area you'd like to give feedback on \n";
+    $response .= "1. Training \n";
+    $response .= "2. Dining \n";
+    $response .= "3. Accomodation \n";
+    $response .= "4. Conferencing \n";
+    $response .= "5. Library services \n";
+    $response .= "6. Security \n";
+    $response .= "0. Back \n";
+    $response .= " \n";
+    $response .= "00. Main Menu \n";
+    $response .= "000. End Session \n";
+}
+else if ($text == "000") {
+    $response .= "END Thank you for visiting The Kenya School of Government. \n";
+    $response .= "You have successfully logged out. \n";
+}
+//IN THE CASE WHERE USER TEXT IS '1, 1' TRAINING then SMC, OUTPUT should be as follows
+if ($text == "1*1") {
+    $response .= "CON On a scale of 1-5, how would you rate the facilitators? \n";
+    $response .= "1. Very poor \n";
+    $response .= "2. Poor \n";
+    $response .= "3. Satisfactory \n";
+    $response .= "4. Very good \n";
+    $response .= "5. Excellent \n";
+    $response .= "0. Back \n";
+    $response .= " \n";
+    $response .= "00. Main Menu \n";
+    $response .= "000. End Session \n";
+}
+else if ($text == "0") {
+    $response .= "CON Hi, Welcome to The Kenya School of Government. \n";
+    $response .= "Please select the area you'd like to give feedback on \n";
+    $response .= "1. Training \n";
+    $response .= "2. Dining \n";
+    $response .= "3. Accomodation \n";
+    $response .= "4. Conferencing \n";
+    $response .= "5. Library services \n";
+    $response .= "6. Security \n";
+    $response .= "0. Back \n";
+    $response .= " \n";
+    $response .= "00. Main Menu \n";
+    $response .= "000. End Session \n";
+}
+else if ($text == "00") {
+    $response .= "CON Hi, Welcome to The Kenya School of Government. \n";
+    $response .= "Please select the area you'd like to give feedback on \n";
+    $response .= "1. Training \n";
+    $response .= "2. Dining \n";
+    $response .= "3. Accomodation \n";
+    $response .= "4. Conferencing \n";
+    $response .= "5. Library services \n";
+    $response .= "6. Security \n";
+    $response .= "0. Back \n";
+    $response .= " \n";
+    $response .= "00. Main Menu \n";
+    $response .= "000. End Session \n";
+}
+else if ($text == "000") {
+    $response .= "END Thank you for visiting The Kenya School of Government. \n";
+    $response .= "You have successfully logged out. \n";
+}
+//IN THE CASE WHERE USER TEXT IS '1, 1, 1' TRAINING,SMC,then VERY POOR OUTPUT should be as follows
+else if ($text == "1*1*1") {
+    $response .= "CON On a scale of 1-5, to what extent were your personal expectations met? \n";
+    $response .= "1. Not at all \n";
+    $response .= "2. Not sure \n";
+    $response .= "3. Satisfactory \n";
+    $response .= "4. To some extent \n";
+    $response .= "5. Great extent \n";
+    $response .= " \n";
+    $response .= "0. Back \n";
+    $response .= "00. Main Menu \n";
+    $response .= "000. End Session \n";
+}
+//IN THE CASE WHERE USER TEXT IS '1, 1, 1, 1" TRAINING,SMC,VERY POOR then NOT AT ALL OUTPUT should be as follows
+else if ($text == "1*1*1*1") {
+    $response .= "CON On a scale of 1-5, how would you rate the facilities (lecture venues, projectors, stationeries e.t.c)? \n";
+    $response .= "1. Very poor \n";
+    $response .= "2. Poor \n";
+    $response .= "3. Satisfactory \n";
+    $response .= "4. Very good \n";
+    $response .= "5. Excellent \n";
+    $response .= "0. Back \n";
+    $response .= " \n";
+    $response .= "00. Main Menu \n";
+    $response .= "000. End Session \n"; 
+}
+//IN THE CASE WHERE USER TEXT IS '1, 1, 1, 1, 1" TRAINING,SMC,VERY POOR, NOT AT ALL then VERY POOR OUTPUT should be as follows
+else if ($text == "1*1*1*1*1") {
+    $response .= "END Thank you. Your feedback has been recorded. \n";
+    $response .= "0. Back \n";
+    $response .= " \n";
+    $response .= "00. Main Menu \n";
+    $response .= "000. End Session \n";
+}
 
 /// END OF TRAINING FEEDBACK SECTION
 
